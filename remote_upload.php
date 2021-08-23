d3be5c3f5da08c94e9f4a45383c961abcba498f0
KamocyTV <harisulumam@gmail.com>
2021-08-23 07:56:18 +0700

    Update remote_upload.php

--- remote_upload.php
+++ remote_upload.php
@@ -1,20 +1,17 @@
 <?php
 
-$login = ''; // L 'API Login openload
-$key = '';   // L 'API key openload
-$urls = ''; // Video URL
+$login = '';
+$key = '';
+$urls = '';
 
 	$curl = curl_init();
 	$opts = [
 		CURLOPT_URL => 'https://api.streamtape.com/remotedl/add?login='.$login.'&key='.$key.'&url='.$url.'',
+		CURLOPT_RETURNTRANSFER => true,
 	];
 	curl_setopt_array($curl, $opts);
 	$response = json_decode(curl_exec($curl), true);
-	
-	echo '<pre>';
-	print_r($response);
-	echo '</pre>';
-	
+	//echo '<pre>';print_r($response);echo '</pre>';
 	$id = $response['result']['id'];
 	
 	$curl2 = curl_init();
@@ -24,12 +21,8 @@ $urls = ''; // Video URL
 	];
 	curl_setopt_array($curl2, $opts);
 	$response = json_decode(curl_exec($curl2), true);
+        //echo '<pre>';print_r($response);echo '</pre>';
+	$dl = $response['result'][''.$id.'']['url'];
 	
-	echo '<pre>';
-	print_r($response);
-	echo '</pre>';
-	
-	$lien = $response['result'][''.$id.'']['url'];
-	
-	echo $lien;
+	echo $dl;
 ?>
